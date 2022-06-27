823353c7ba8fddf381c5583cf14d5263714cff2f
Seong-Yong <58159831+Seong-Yong@users.noreply.github.com>
2022-06-27 22:00:05 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+
+	cin >> a;
+	cin >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+}
