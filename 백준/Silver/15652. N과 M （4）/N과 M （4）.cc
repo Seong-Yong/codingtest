1d9dd0ff79d9626e3ff20382dc011a1c444ac6d5
Seong-Yong <58159831+Seong-Yong@users.noreply.github.com>
2022-10-23 23:46:11 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+vector<int> result;
+int n, m;
+bool list[8] = { 0, };
+int arr[8] = { 0, };
+void nandm(int a, int b)
+{
+	if (b == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << '\n';
+		return;
+	}
+
+	for (int i = a; i <= n; i++) {
+		arr[b] = i;
+		nandm(i , b + 1);
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	nandm(1, 0);
+
+}
